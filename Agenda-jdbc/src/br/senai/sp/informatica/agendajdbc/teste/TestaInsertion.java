1775fac9ef14fee039a04169db46b2efa568e6f0
Rafael Thayto <rafa.thayto@gmail.com>
2017-09-21 16:32:03 -0300

    Added nem classes and packages to agenda-jdbc

---
+++ Agenda-jdbc/src/br/senai/sp/informatica/agendajdbc/teste/TestaInsertion.java
@@ -0,0 +1,28 @@
+package br.senai.sp.informatica.agendajdbc.teste;
+
+import java.util.Calendar;
+
+import br.senai.sp.informatica.agendajdbc.dao.ContatoDao;
+import br.senai.sp.informatica.agendajdbc.model.Contato;
+
+public class TestaInsertion {
+
+	public static void main(String[] args) {
+		
+		// Cria um Contato
+		Contato contato = new Contato();
+		contato.setNome("Front-end");
+		contato.setEmail("javascript@html.css");
+		contato.setEndereco("No seu Coração / Browser");
+		// Calendar.getInstance() = data de hoje !
+		contato.setDataNascimento(Calendar.getInstance());
+		
+
+		// Cria uma instancia de ContatoDao
+		// abre uma conexão com o banco de dados
+		ContatoDao dao = new ContatoDao();
+		dao.salva(contato);
+		System.out.println("Salvo !");
+	}
+
+}
